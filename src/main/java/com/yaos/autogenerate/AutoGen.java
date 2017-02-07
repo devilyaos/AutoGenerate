6d391eb0db64606927a80107ad55a5956943e225
devilyaos <198194220@qq.com>
2017-02-07 17:09:11 +0800

    按照迭代的方式重构，先满足java自动生成

--- src/main/java/com/yaos/autogenerate/AutoGen.java
+++ src/main/java/com/yaos/autogenerate/AutoGen.java
@@ -1,42 +1,135 @@
 package com.yaos.autogenerate;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.jfinal.kit.PathKit;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
- * 生成器对外入口
  * @AUTHOR yaos
  * @DATE 2017-02-06
  */
 public class AutoGen {
 
-    private static GenService genService = new GenService() ;
+    private static AutoGen instance = new AutoGen();
+    private JSONObject config ;
 
     /**
-     * 设置配置项
-     * @param _config 配置项
+     * 获取单例
+     * @return 单例对象
      */
-    public static void config(AutoGenConfig _config){
-        genService.setAutoGenConfig(_config) ;
+    public static AutoGen use(){
+        return instance ;
     }
 
     /**
-     * 设定批量生成的模板，适合于多模版统一数据源
-     * @param templates 模板名称
+     * 初始化
+     * @return 链式调用
      */
-    public static void use(String... templates){
-        genService.addTemplates(templates);
+    public AutoGen init(){
+        init(PathKit.getRootClassPath() + "/defaultRule.json");
+        return this ;
     }
 
     /**
-     * 增加数据源
-     * @param _params 数据源
+     * 初始化
+     * @param configPath 配置路径
+     * @return 链式调用
      */
-    public static void with(DbTablesData _params){
-        genService.setParams(_params) ;
+    public AutoGen init(String configPath){
+        if(configPath == null || configPath.length() == 0){
+            configPath = PathKit.getRootClassPath() + "/defaultRule.json" ;
+        }
+        config = getConfig(configPath) ;
+        if(config == null){
+            throw new NullPointerException("找不到配置");
+        }
+        return this ;
     }
 
     /**
-     * 开始生成
+     * 创建文件的服务
      */
-    public static void build() throws Exception{
-        genService.start();
+    public void create(){
+        String[] tableNames = (String[]) config.getJSONArray("tableName").toArray();
+        for(String tableName : tableNames){
+            if(tableName == null || tableName.length() == 0){
+                continue;
+            }
+            foreachCreatation(tableName);
+        }
+    }
+
+    /**
+     * 循环创建服务
+     * @param tableName 表名
+     */
+    private void foreachCreatation(String tableName){
+
+    }
+
+    /**
+     * 获取配置
+     * @param path 配置路径
+     * @return 配置对象
+     */
+    private JSONObject getConfig(String path){
+        BufferedReader reader = null;
+        StringBuffer laststr = new StringBuffer("");
+        try{
+            FileInputStream fileInputStream = new FileInputStream(path);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+            reader = new BufferedReader(inputStreamReader);
+            String tempString = null;
+            while((tempString = reader.readLine()) != null){
+                laststr.append(tempString);
+            }
+            reader.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            if(reader != null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if(laststr.length() > 0){
+            return JSON.parseObject(laststr.toString()) ;
+        }else{
+            return null ;
+        }
+    }
+
+    /**
+     * 数据库名称按照小驼峰转换成参数名称
+     * @param jdbcName 数据库名称
+     * @return 参数名称
+     */
+    private String transJdbcName2ParaName(String jdbcName){
+        String[] arr = jdbcName.split("_") ;
+        if(arr == null || arr.length == 0){
+            return "undefined" ;
+        }
+        String paraName = arr[0] ;
+        for(int i = 1 , len = arr.length ; i < len ; i++){
+            paraName += upFirstLetter(paraName);
+        }
+        return paraName ;
+    }
+
+    /**
+     * 将单词的首字母大写
+     * @param word 需要转换的单词
+     * @return 抓换后的单词
+     */
+    private String upFirstLetter(String word){
+        return word.substring(0,1).toUpperCase() + word.substring(1) ;
     }
 }
