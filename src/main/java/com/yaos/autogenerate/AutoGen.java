3603c21d4b6ef86f058aceee54add794e50280d5
devilyaos <198194220@qq.com>
2017-02-08 15:55:29 +0800

    完善功能，由于jfinal内置的模板会有莫名的空行，所以切换到beetl

--- src/main/java/com/yaos/autogenerate/AutoGen.java
+++ src/main/java/com/yaos/autogenerate/AutoGen.java
@@ -1,13 +1,32 @@
 package com.yaos.autogenerate;
 
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.wall.WallConfig;
+import com.alibaba.druid.wall.WallFilter;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.jfinal.kit.JMap;
 import com.jfinal.kit.PathKit;
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.activerecord.CaseInsensitiveContainerFactory;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+import com.jfinal.plugin.activerecord.dialect.MysqlDialect;
+import com.jfinal.plugin.druid.DruidPlugin;
+import com.jfinal.template.Engine;
+import org.apache.commons.io.FileUtils;
+import org.beetl.core.Configuration;
+import org.beetl.core.GroupTemplate;
+import org.beetl.core.Template;
+import org.beetl.core.resource.FileResourceLoader;
+import org.joda.time.DateTime;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @AUTHOR yaos
@@ -17,6 +36,11 @@ public class AutoGen {
 
     private static AutoGen instance = new AutoGen();
     private JSONObject config ;
+    private DruidPlugin dp ;
+    private ActiveRecordPlugin arp ;
+    private FileResourceLoader resourceLoader ;
+    private Configuration cfg ;
+    private GroupTemplate gt ;
 
     /**
      * 获取单例
@@ -30,7 +54,7 @@ public class AutoGen {
      * 初始化
      * @return 链式调用
      */
-    public AutoGen init(){
+    public AutoGen init() throws IOException {
         init(PathKit.getRootClassPath() + "/defaultRule.json");
         return this ;
     }
@@ -40,7 +64,7 @@ public class AutoGen {
      * @param configPath 配置路径
      * @return 链式调用
      */
-    public AutoGen init(String configPath){
+    public AutoGen init(String configPath) throws IOException {
         if(configPath == null || configPath.length() == 0){
             configPath = PathKit.getRootClassPath() + "/defaultRule.json" ;
         }
@@ -48,28 +72,97 @@ public class AutoGen {
         if(config == null){
             throw new NullPointerException("找不到配置");
         }
+        JSONObject dataSourceConfig = config.getJSONObject("dataSource") ;
+        dp = new DruidPlugin(
+                dataSourceConfig.getString("url"),
+                dataSourceConfig.getString("user"),
+                dataSourceConfig.getString("pwd")
+        );
+        dp.addFilter(new StatFilter());
+        WallFilter wall = new WallFilter();
+        wall.setDbType("mysql");
+        WallConfig wallConfig = new WallConfig();
+        wallConfig.setFunctionCheck(false);
+        wallConfig.setCreateTableAllow(true);
+        wallConfig.setCommentAllow(true);
+        wallConfig.setMultiStatementAllow(true);
+        wall.setConfig(wallConfig);
+        dp.addFilter(wall);
+        arp = new ActiveRecordPlugin(dp);
+        arp.setContainerFactory(new CaseInsensitiveContainerFactory(true));// 大小写不敏感
+        arp.setDialect(new MysqlDialect());
+        dp.start();
+        arp.start();
+        resourceLoader = new FileResourceLoader(config.getString("baseTemplatePath"),"utf-8");
+        cfg = Configuration.defaultConfiguration();
+        gt = new GroupTemplate(resourceLoader, cfg);
+        System.out.println("-- 初始化成功");
         return this ;
     }
 
     /**
      * 创建文件的服务
      */
-    public void create(){
-        String[] tableNames = (String[]) config.getJSONArray("tableName").toArray();
-        for(String tableName : tableNames){
-            if(tableName == null || tableName.length() == 0){
-                continue;
+    public void create() throws IOException {
+        JSONArray tables = config.getJSONArray("tables") ;
+        JSONArray templates = config.getJSONArray("templates") ;
+        Map<String,Object> params ;
+        JSONObject table ;
+        StringBuffer columnLineStr = new StringBuffer("");
+        JSONObject template ;
+        String text ;
+        File genFile ;
+        String filePath ;
+        List<Record> columnList ;
+        Template beetlTemplate ;
+        //循环表
+        for(int i = 0 ,len = tables.size() ; i < len ; i++){
+            params = new HashMap<String,Object>();
+            table = tables.getJSONObject(i) ;
+            System.out.println(String.format("-- 开始生成 %s 相关文件",table.getString("tableName")));
+            //设置全局变量
+            params.put("author",config.getString("author")) ;
+            params.put("now",DateTime.now().toString(config.getString("dateFormat"))) ;
+            //设置表信息
+            params.put("tableName",table.getString("tableName")) ;
+            params.put("tableComment",table.getString("tableComment")) ;
+            params.put("tableMethodName",upFirstLetter(transJdbcName2ParaName(
+                    table.getString("tableName").replace(table.getString("tablePre"),"")
+            )));
+            //设置列信息
+            columnList = Db.find(String.format("show full columns from %s",table.getString("tableName")));
+            for(Record record : columnList){
+                record.set("ColumnName",record.getStr("Field"));
+                record.set("CodeType",transJdbcType2CodeType(record.getStr("Type"))) ;
+                record.set("CodeParamName",transJdbcName2ParaName(record.getStr("Field").replace("is_","")));
+                record.set("CodeMethodName",upFirstLetter(transJdbcName2ParaName(record.getStr("Field").replace("is_",""))));
+                columnLineStr.append(record.getStr("CodeType")).append(" ").append(record.getStr("CodeParamName")).append(",") ;
             }
-            foreachCreatation(tableName);
+            params.put("columnList",columnList) ;
+            params.put("columnLine",columnLineStr.length() > 0 ?
+                    columnLineStr.substring(0,columnLineStr.lastIndexOf(",")) : "") ;
+            //循环模板
+            for(int j = 0 , size = templates.size() ; j < size ; j++){
+                template = templates.getJSONObject(j) ;
+                params.put("package",template.getString("package")) ;
+                params.put("comment",template.getString("comment")) ;
+                beetlTemplate = gt.getTemplate(template.getString("templateName"));
+                beetlTemplate.binding(params);
+                text = beetlTemplate.render() ;
+//                text = Engine.use()
+//                        .setBaseTemplatePath(config.getString("baseTemplatePath"))
+//                        .getTemplate(template.getString("templateName")).renderToString(params);
+                filePath = template.getString("filePath").replace("{name}",params.get("tableMethodName").toString()) ;
+                genFile = new File(filePath) ;
+                FileUtils.touch(genFile);
+                FileUtils.write(genFile,text,Charset.forName("utf-8"));
+                System.out.println(String.format("---- %s : %s Done!",table.getString("tableName"),template.getString("templateName")));
+            }
+            System.out.println(String.format("-- %s 处理结束",table.getString("tableName")));
         }
-    }
-
-    /**
-     * 循环创建服务
-     * @param tableName 表名
-     */
-    private void foreachCreatation(String tableName){
-
+        arp.stop();
+        dp.stop();
+        System.out.println("-- 成功释放");
     }
 
     /**
@@ -104,6 +197,25 @@ public class AutoGen {
             return JSON.parseObject(laststr.toString()) ;
         }else{
             return null ;
+        }
+    }
+
+    /**
+     * 将数据库类型转换成代码类型
+     * @param jdbcType 数据库类型
+     * @return 代码类型
+     */
+    private String transJdbcType2CodeType(String jdbcType){
+        if(jdbcType == null || jdbcType.length() == 0){
+            return "undefined" ;
+        }
+        if(jdbcType.indexOf("(") > 0){
+            jdbcType = jdbcType.substring(0,jdbcType.indexOf("("));
+        }
+        if(config.getJSONObject("columnType").containsKey(jdbcType)){
+            return config.getJSONObject("columnType").getString(jdbcType);
+        }else{
+            return "undefined" ;
         }
     }
 
